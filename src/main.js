a679aa91c0db3820fe72e763a574bc57f95ff9ef
tacsi566 <zsolt.zombori11@gmail.com>
2022-04-03 23:03:35 +0200

    Added Scroll to the top button.

--- src/main.js
+++ src/main.js
@@ -6,11 +6,19 @@ import Vuetify from "vuetify";
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
-
+import vueToTopButton from "vue-to-top-button"
 
 Vue.config.productionTip = false
 document.title = "MyRecipes"
 
+Vue.use(vueToTopButton, {
+
+  // Change type to 0 to disable animation
+  type: 1,
+  color: "#2178ff",
+  //bg: "#ffffff",
+  offset: 2
+})
 
 Vue.use(Vuetify);
 new Vue({
