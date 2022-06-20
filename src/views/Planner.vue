2f17f0ffa2480790e0caaa5c09ed1172560a561c
Denis Radovanovic <denis.radovanovic@stud.fh-campuswien.ac.at>
2022-06-20 16:52:14 +0200

    Removed style block from Planner.vue

--- src/views/Planner.vue
+++ src/views/Planner.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h2 class="text-h4 font-weight-bold">Food Planner</h2>
+    asdf
     <planner></planner>
   </v-container>
 </template>
@@ -15,5 +16,3 @@ export default {
   },
 };
 </script>
-<style>
-</style>
