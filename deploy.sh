11c433a7787bc7124f42291a5206f98eae8484da
Denis Radovanovic <radovanovic.denis@web.de>
2022-12-12 19:04:44 +0100

    Add shebang to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export DBPASSWORD=$(ansible-vault view vault.yml --vault-password-file vault.key | grep mariadb_root_password | awk '{print $2}')
 sed -ie "s/MARIADB_ROOT_PASSWORD:\ example/MARIADB_ROOT_PASSWORD:\ $DBPASSWORD/g" docker-compose.yml
 rm docker-compose.ymle
