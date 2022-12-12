2402e997c247ddad61d7c88c3eb72c408064869e
Denis Radovanovic <radovanovic.denis@web.de>
2022-12-12 19:04:11 +0100

    Add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+export DBPASSWORD=$(ansible-vault view vault.yml --vault-password-file vault.key | grep mariadb_root_password | awk '{print $2}')
+sed -ie "s/MARIADB_ROOT_PASSWORD:\ example/MARIADB_ROOT_PASSWORD:\ $DBPASSWORD/g" docker-compose.yml
+rm docker-compose.ymle
+docker-compose up -d --build
+sed -ie "s/MARIADB_ROOT_PASSWORD:\ $DBPASSWORD/MARIADB_ROOT_PASSWORD:\ example/g" docker-compose.yml
+rm docker-compose.ymle
