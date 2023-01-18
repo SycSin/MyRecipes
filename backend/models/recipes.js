543503d9e6abf0515077de54e3251cd486ba2df0
Denis Radovanovic <radovanovic.denis@web.de>
2023-01-18 21:30:42 +0100

    Add recipe model

---
+++ backend/models/recipes.js
@@ -0,0 +1,75 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: 'db',
+    user: 'root',
+    password: 'example',
+    database: 'MyRecipes',
+    connectionLimit: 5
+});
+
+const Recipe = {
+    async getAllRecipes() {
+        try {
+            const conn = await pool.getConnection();
+            const rows = await conn.query('SELECT * FROM recipes');
+            conn.release();
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    async getRecipeById(id) {
+        try {
+            const conn = await pool.getConnection();
+            const rows = await conn.query('SELECT * FROM recipes WHERE recipes_UID = ?', [id]);
+            conn.release();
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    async addRecipe(recipe) {
+        try {
+            const conn = await pool.getConnection();
+            const rows = await conn.query(
+                'INSERT INTO recipes (author, title, description, ingredients, steps, date, image, rating, category) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                [recipe.author, recipe.title, recipe.description, recipe.ingredients, recipe.steps, recipe.date, recipe.image, recipe.rating, recipe.category]
+            );
+            conn.release();
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    async updateRecipe(id, recipe) {
+        try {
+            const conn = await pool.getConnection();
+            const rows = await conn.query(
+                'UPDATE recipes SET author = ?, title = ?, description = ?, steps = ?, date = ?, image = ?, rating = ?, category = ? WHERE recipes_UID = ?',
+                [recipe.author, recipe.title, recipe.description, recipe.ingredients, recipe.steps, recipe.date, recipe.image, recipe.rating, recipe.category]
+            );
+            conn.release();
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    async deleteRecipe(id) {
+        try {
+            const conn = await pool.getConnection();
+            const rows = await conn.query('DELETE FROM recipes WHERE recipes_UID = ?', [id]);
+            conn.release();
+            return rows;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+};
+
+module.exports = Recipe;
---
+++ backend/routes/recipes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve users', error });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.getUserById(req.params.id);
+    if (!user.length) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve user', error });
+  }
+});
+
+router.post('/', async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).json({ message: 'Please provide an email, and password' });
+  } else {
+    try {
+      const newUser = await User.addUser(req.body);
+      res.status(200).json({ message: 'User added'});
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to add user', error });
+    }
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  if (!req.body.email && !req.body.password) {
+    res.status(400).json({ message: 'Please provide at least one field to update' });
+  } else {
+    try {
+      const updatedUser = await User.updateUser(req.params.id, req.body);
+      if (updatedUser.affectedRows === 0) {
+        res.status(404).json({ message: 'User not found' });
+      } else {
+        res.status(200).json({ message: 'User updated', updatedUser });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to update user', error });
+    }
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.deleteUser(req.params.id);
+    if (deletedUser.affectedRows === 0) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.status(200).json({ message: 'User deleted', deletedUser });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to delete user', error });
+  }
+});
+
+module.exports = router;
